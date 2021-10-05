4905669ea6f63c6b711f1f7f7b241de522cd4f12
Oluwatobi Adegoke <adegoketobi5496@gmail.com>
2021-10-05 13:52:42 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      primary: "Source Sans Pro",
+      secondary: "Playfair Display",
+      neut: "Abril Fatface",
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
